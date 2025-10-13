2319985e4f1e769f934f7ca948330af26ab66cd7
Edwin32322 <edwincastellanos150@gmail.com>
2025-10-13 12:40:56 -0500

    add routes in app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+  path: 'home',
+  loadChildren: () => import('./modules/home/home.routes').then(r => r.homeRoutes)
+},
+{
+  path: 'users',
+  loadChildren: () => import('./modules/users/users.routes').then(r => r.userRoutes)
+},
+{
+  path: '**',
+  redirectTo: 'users'
+}
+];
