78a21758fe72c03f745e9c592c0eb041cd1e8c26
Edwin32322 <edwincastellanos150@gmail.com>
2025-10-13 12:34:48 -0500

    create homes routes

---
+++ src/app/modules/home/home.routes.ts
@@ -0,0 +1,7 @@
+import { Route } from "@angular/router";
+import { HomeComponent } from "./pages/home/home.component";
+
+export const homeRoutes: Route[] = [
+{path: "", component: HomeComponent},
+{path: "**", redirectTo: ""}
+]
